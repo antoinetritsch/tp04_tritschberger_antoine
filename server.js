541c30425bd07d33f7c12b36bff7c4caca6ddb74
tritschbergera <antoine.tritschberger@etu.unistra.fr>
2022-01-29 08:50:12 +0100

    correction server

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-app.use(express.static(__dirname + "/dist/antoine-tritschberger-app"));
+app.use(express.static(__dirname + "/dist/tp04_tritschberger_antoine"));
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/antoine-tritschberger-app/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/tp04_tritschberger_antoine/index.html"));
 });
 app.listen(process.env.PORT || 8080);
